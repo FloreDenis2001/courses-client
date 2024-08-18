d9b65ea119b6ac0778ddbc16edde3f5036c51ecd
FloreDenis2001 <floredenis907@yahoo.com>
2024-08-19 02:35:11 +0300

    update

--- components/Header.tsx
+++ components/Header.tsx
@@ -28,7 +28,7 @@ const Header = () => {
     <header className="fixed top-0 left-0 right-0 z-50 mx-4 mt-2">
       <div
         className={`flex flex-row items-center rounded-xl h-20 w-full max-w-screen-lg mx-auto px-4 py-2 transition-all duration-300 ${
-          scrolled ? "bg-secondary border-2 border-primary shadow-md" : "bg-secondary"
+          scrolled ? "bg-secondary border-2 border-b-primary shadow-md" : "bg-secondary"
         }`}
       >
         <BurgerMenu />
